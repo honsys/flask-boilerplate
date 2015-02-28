ce0ec1575f5aa312ccec721cbe3366bd4f097530
David B. Hon <davidb.hon@gmail.com>
2015-02-28 11:01:27 -0500

    more prose and/or code edits

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 
 from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
-import logging
+import os, logging
 from logging import Formatter, FileHandler
 from forms import *
 
@@ -21,20 +21,20 @@ app.config.from_object('config')
 '''
 @app.teardown_request
 def shutdown_session(exception=None):
-    db_session.remove()
+  db_session.remove()
 '''
 
 # Login required decorator.
 '''
 def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
+  @wraps(test)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return test(*args, **kwargs)
+    else:
+      flash('You need to login first.')
+      return redirect(url_for('login'))
+  return wrap
 '''
 #----------------------------------------------------------------------------#
 # Controllers.
@@ -43,65 +43,66 @@ def login_required(test):
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
+  return render_template('pages/placeholder.home.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('pages/placeholder.about.html')
+  return render_template('pages/placeholder.about.html')
 
 
 @app.route('/login')
 def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
+  form = LoginForm(request.form)
+  return render_template('forms/login.html', form=form)
 
 
 @app.route('/register')
 def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
+  form = RegisterForm(request.form)
+  return render_template('forms/register.html', form=form)
 
 
 @app.route('/forgot')
 def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
+  form = ForgotForm(request.form)
+  return render_template('forms/forgot.html', form=form)
 
 # Error handlers.
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    #db_session.rollback()
-    return render_template('errors/500.html'), 500
+  #db_session.rollback()
+  return render_template('errors/500.html'), 500
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+  return render_template('errors/404.html'), 404
 
 if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+  file_handler = FileHandler('error.log')
+  file_handler.setFormatter(
+    Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+  )
+  app.logger.setLevel(logging.INFO)
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
+  app.logger.info('errors')
 
 #----------------------------------------------------------------------------#
 # Launch.
 #----------------------------------------------------------------------------#
 
 # Default port:
+'''
 if __name__ == '__main__':
-    app.run()
+  app.run()
+'''
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+  flaskport = int(os.environ.get('FLASKPORT', 9901))
+# app.run(host='localhost', port=flaskport)
+  app.run(port=flaskport)
